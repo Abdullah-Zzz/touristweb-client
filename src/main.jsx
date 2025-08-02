90f293aacf79aa1cce4158707be58a7fe1b54795
abdullah-zzz <ak0344494@gmail.com>
2025-08-03 03:36:10 +0500

    fixed nav

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
 import ScrollToTop from './Components/scroll/scrollToTop.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <ScrollToTop />
       <App />
     </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
